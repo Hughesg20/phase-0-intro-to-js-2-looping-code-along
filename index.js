7fbd364a963cc5d2c4cfe365033957e77e7659bb
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-06 15:28:47 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names,giftType){
+    var newArray=[]
+     for(let i=0;i<names.length;i++){
+     newArray.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+ 
+     }
+ return newArray
+ }
+
+ function countDown(n){
+    let i = n;
+    while (i >= 0 ) {
+        console.log(i);
+        i= i - 1
+    }
+}
